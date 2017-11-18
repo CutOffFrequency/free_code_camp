9864879ee064e7fc3804e754489d60004e5cb2e4
Erik Waters <erikj.waters@gmail.com>
2017-11-18 12:46:35 -0500

    some work

--- intermediateAlgorithmScripting/03_romanNumeralConverter.js
+++ intermediateAlgorithmScripting/03_romanNumeralConverter.js
@@ -17,48 +17,50 @@ function convertToRoman(num) {
             tens: "hundreds",
             hundreds: "thousands"
         }
-        one = numerals.place.one;
+        one = numerals[place].one;
         two = one + one;
         three = two + one;
-        five = numerals.place.five;
+        five = numerals[place].five;
         four = one + five;
         six = five + one;
         seven = five + two;
         eight = five + three;
-        next = numerals.[getNext[place]].one
+        // next = numerals.[getNext[place]].one
+        next = numerals[ getNext[place] ]
         nine = one + next;
         let getSwitch = (digit) => {
             switch(digit) {
-                case 0:
+                case '0':
                     return "";
-                case 1:
+                case '1':
                     return one;
-                case 2: 
+                case '2':
                     return two;
-                case 3:
+                case '3':
                     return three;
-                case 4: 
+                case '4':
                     return four;
-                case 5:
+                case '5':
                     return five;
-                case 6:
+                case '6':
                     return six;
-                case 7: 
+                case '7':
                     return seven;
-                case 8:
+                case '8':
                     return eight;
-                case 9:
+                case '9':
                     return nine;
                 default:
-                    alert("error in switch")
+                    console.log("error in switch", digit)
             }
         }
         return getSwitch(digit);
     }
- return num;
+    let getPlace = (i, l) => {
+        let places = ["thousands","hundreds","tens","ones"]
+        return places[4-l]
+    }
+    return num.toString().split('').map((digit, index, arr)=>convert(digit, getPlace(index, arr.length))).join('')
 }
-
-
-
 
 convertToRoman(36);
