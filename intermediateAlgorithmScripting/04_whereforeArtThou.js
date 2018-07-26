f38e90f77943708b1c4f2ca7383367f04e255703
Erik Waters <erikj.waters@gmail.com>
2018-07-26 18:10:57 -0400

    completed int 04

---
+++ intermediateAlgorithmScripting/04_whereforeArtThou.js
@@ -0,0 +1,21 @@
+// Make a function that looks through an array of objects (first argument)
+// and returns an array of all objects that have matching name and value pairs
+// (second argument). Each name and value pair of the source object has to be
+// present in the object from the collection if it is to be included in the
+// returned array.
+
+function whatIsInAName(collection, source) {
+  var arr = [];
+
+  let sourceKeys = Object.keys(source);
+
+  arr = collection.filter(el =>
+    sourceKeys.every(key => el.hasOwnProperty(key) && el[key] === source[key])
+  );
+
+  return arr;
+}
+
+console.log(
+  whatIsInAName([{ a: 1, b: 2 }, { a: 1, b: 3 }, { b: 3 }], { a: 1 })
+);
