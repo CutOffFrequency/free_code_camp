b8b44eae3e5920fd9abcfe7789b4c64925f17a8e
Erik Waters <erikj.waters@gmail.com>
2018-07-28 14:37:53 -0400

    finished int. 05

---
+++ intermediateAlgorithmScripting/05_thisIsSpinalCase.js
@@ -0,0 +1,17 @@
+let spinalCase = str =>
+  str.split("").reduce((a, b, c, d) => {
+    return (
+      a +
+      (c == 0
+        ? b.toLowerCase()
+        : b.replace(/[_\W]/, "-") == "-"
+          ? "-"
+          : b == b.toUpperCase()
+            ? d[c - 1].replace(/[_\W]/, "-") != "-"
+              ? "-" + b.toLowerCase()
+              : b.toLowerCase()
+            : b)
+    );
+  }, "");
+
+console.log(spinalCase("ThisIs_Spinal Tap"));
