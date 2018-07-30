f9a36d3ca39f36060cc46c3eef8b34ecc57ae674
Erik Waters <erikj.waters@gmail.com>
2018-07-30 18:56:38 -0400

    completed int 06

---
+++ intermediateAlgorithmScripting/06_pigLatin.js
@@ -0,0 +1,22 @@
+// function translatePigLatin(str) {
+//   let clusters = ['bl', 'br', 'ch', 'ck', 'cl', 'cr', 'dr', 'fl', 'fr', 'gh', 'gl', 'gr', 'ng', 'ph', 'pl', 'pr', 'qu', 'sc', 'sh', 'sk', 'sl', 'sm', 'sn', 'sp', 'st', 'sw', 'th', 'tr', 'tw', 'wh', 'wr']
+//   let shift = vIndex => {
+//     return str.slice(vIndex, str.length) + str.slice(0, vIndex) + 'ay'
+//   }
+//   return str.search(/[a,e,i,o,u]/) === 0 ?
+//   str + 'way' :
+//     str.search(/[a,e,i,o,u]/) > 0 ?
+//       shift(str.search(/[a,e,i,o,u]/)) :
+//       clusters.indexOf(str.slice(0,2) > -1) ?
+//       shift(2) :
+//       shift(1)
+// }
+
+let translatePigLatin = str => {
+  return str
+    .replace(/^([aeiou])(.*)/, "$1$2way")
+    .replace(/^([^aeiou]+)(.*)/, "$2$1ay");
+};
+
+console.log(translatePigLatin("dang"));
+console.log(translatePigLatin("ohyeah"));
