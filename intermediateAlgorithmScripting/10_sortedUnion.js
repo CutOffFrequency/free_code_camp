a6f0f589e0b36cdaff8782ecd8d78b88b065ef2b
Erik Waters <erikj.waters@gmail.com>
2018-08-02 15:30:07 -0400

    update int 10

--- intermediateAlgorithmScripting/10_sortedUnion.js
+++ intermediateAlgorithmScripting/10_sortedUnion.js
@@ -1,7 +1,7 @@
+/* my original answer */
 function uniteUnique(arr) {
-  arr = [...arguments];
   let aSet = new Set([]);
-  arr = arr.reduce((a, b) => a.concat(b));
+  arr = [...arguments].reduce((a, b) => a.concat(b));
   return arr.reduce((a, b) => {
     if (typeof b == "number") {
       if (!aSet.has(b)) {
@@ -19,5 +19,19 @@ function uniteUnique(arr) {
     }
   }, []);
 }
-
 console.log(uniteUnique([1, 3, 2], [5, 2, 1, 4], [2, 1]));
+
+// the solution below  is of course more concise so I have updated it
+// HOWEVER
+// the solution below yeilds a result with unique numerical values for each
+// arguments array presented for this challenge only because the arguments arrays
+// presented do not contain duplicate values at different depths!
+// calling the function like so:
+// uniteUnique([1, 3, 2], [[5], 2, 1, 4], [2, 1, 5]))
+// would yield a result with values: 5 & [5] which are of course, not unique values
+// according to my original interpretation of the challenge
+
+// let uniteUnique = arr => {
+//   const args = [].concat(...arguments);
+//   return [...new Set(args)];
+// };
