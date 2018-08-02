324eac64eed394d785da6488fa42f06469477349
Erik Waters <erikj.waters@gmail.com>
2018-08-02 15:56:12 -0400

    complete int 11

---
+++ intermediateAlgorithmScripting/11_convertHTMLEntities.js
@@ -0,0 +1,14 @@
+let convertHTML = str =>
+  str.split("").reduce((a, b) => {
+    return (
+      a +
+      b
+        .replace("&", "&amp;")
+        .replace("<", "&lt;")
+        .replace(">", "&gt;")
+        .replace('"', "&quot;")
+        .replace("'", "&apos;")
+    );
+  }, "");
+
+console.log(convertHTML("Hamburgers < Pizza < Tacos"));
