4224df6f3246d4d2a67297329c8c9d9fd8e1b501
Erik Waters <erikj.waters@gmail.com>
2018-08-04 15:55:45 -0400

    improved int 13

--- intermediateAlgorithmScripting/13_sumAllPrimes.js
+++ intermediateAlgorithmScripting/13_sumAllPrimes.js
@@ -1,17 +1,18 @@
 function sumPrimes(num) {
-  let primes = [],
-    p = 2;
+  let primes = [2],
+    p = 3;
   if (num < 2) return 0;
   // primes are tested using the trial division method with some optimizations:
   // we only need to check for prime factors up to the target number's square root
+  // each prime after 2 is odd so we can increment the counter by 2
   const isPrime = num => {
     for (let i = 0, s = Math.sqrt(num); primes[i] <= s; i++)
       if (num % primes[i] === 0) return false;
-    return num >= 2;
+    return num > 2;
   };
   while (p <= num) {
     if (isPrime(p)) primes.push(p);
-    p += 1;
+    p += 2;
   }
   return primes.reduce((a, b) => a + b);
 }
