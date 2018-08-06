35192f0a5e6a269ecb422d0bb4f1c15d39815b55
Erik Waters <erikj.waters@gmail.com>
2018-08-06 16:26:33 -0400

    complete int 19

---
+++ intermediateAlgorithmScripting/19_argumentsOptional.js
@@ -0,0 +1,13 @@
+function addTogether() {
+  let args = [...arguments];
+  let validateNumericArg = x => typeof x == "number";
+  let func = (x, y) => {
+    if (validateNumericArg(y)) return x + y;
+    return undefined;
+  };
+  return args.length == 1 && validateNumericArg(args[0])
+    ? y => func(args[0], y)
+    : func(args[0], args[1]);
+}
+
+console.log(addTogether(2, 3));
