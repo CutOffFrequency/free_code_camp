751624b4b0015cccbfaceabd2ca1ac7ea13775b4
Erik Waters <erikj.waters@gmail.com>
2018-08-06 15:35:49 -0400

    complete int 18

---
+++ intermediateAlgorithmScripting/18_everythingBeTrue.js
@@ -0,0 +1,15 @@
+let truthCheck = (collection, pre) => {
+  return collection.every(el => (el[pre] ? true : false));
+};
+
+console.log(
+  truthCheck(
+    [
+      { user: "Tinky-Winky", sex: "male" },
+      { user: "Dipsy", sex: "male" },
+      { user: "Laa-Laa", sex: "female" },
+      { user: "Po", sex: "female" }
+    ],
+    "sex"
+  )
+);
